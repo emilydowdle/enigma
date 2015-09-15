274c9e7e4f3f34fd4291245c4bb1be8846a50e6c
Emily Dowdle <emily@editingemily.com>
2015-09-15 16:00:16 -0600

    Updates to rotator function

--- lib/rotator.rb
+++ lib/rotator.rb
@@ -1,33 +1,48 @@
 class Rotator
 
-### we've got two strings coming in that we need to add: the key string and the date offset string
-
   CHAR_MAP = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ","]
 
-  def initialize(string)
-    @string = string
+  attr_accessor :key_array,
+                :offset_array
+
+  def initialize(key_array, offset_array)
+    @key_array = key_array
+    @offset_array = offset_array
   end
 
-  def string_to_array
-    key_array = @string.chars
+  def map_runner
+    find_an_offset
+    create_a_map(a_offset)
+  end
+
+  def find_a_offset
+    key_offset = "#{key_array[0]}#{key_array[1]}".to_i
+    offset = "#{offset_array[0]}".to_i
+    a_offset = key_offset + offset
+  end
+
+  def create_a_map(offset)
+    slide = offset % CHAR_MAP.length
+    temp = []
+    leftovers = []
+    final_map = []
+    CHAR_MAP.each_with_index do |letter, index|
+      temp << CHAR_MAP[index + slide]
+      if !temp.include? letter
+        leftovers << letter
+      end
+      temp.delete(nil)
+      final_map = temp + leftovers
+    end
+    final_map
   end
 
 end
 
+# START
 
-PSEUDOCODE FOR Rotator
-
-START
-
-key string
-break key string into an array (key array)
-
-a
-
-a rotation
-  [b, c, d...]
-b rotation
-  [d, e, f, g]
-c rotation
-
-d rotation
+# create array for each rotation map
+#   - use remainder to slide letters within each map array
+#   - return each map and set a variable
+#
+# END
--- test/test_rotator.rb
+++ test/test_rotator.rb
@@ -5,17 +5,27 @@ require_relative '../lib/rotator'
 
 class TestRotator < Minitest::Test
 
-  def test_code_has_5_digits
-    skip
-    code = Rotator.new
-    assert_equal 5, code.key.length
+  def test_offset_a
+    code = Rotator.new(["3", "4", "5", "6", "7"], ["6", "9", "1", "2"])
+    assert_equal 40, code.find_a_offset
   end
 
   def test_2_codes_are_not_same
-    skip
-    code = Rotator.new
-
-    assert first_code != second_code
+    code = Rotator.new(["9", "0", "5", "6", "7"], ["1", "5", "1", "2"])
+    assert_equal 91, code.find_a_offset
   end
+
+  def test_create_a_map
+    code = Rotator.new(["9", "0", "5", "6", "7"], ["1", "5", "1", "2"])
+    assert_equal "g", code.create_a_map(45)[0]
+  end
+
+  def test_create_map_different_slide
+    code = Rotator.new(["9", "0", "5", "6", "7"], ["1", "5", "1", "2"])
+    assert_equal "1", code.create_a_map(105)[0]
+  end
+
+
+
 
 end
