173882662b1e2311cb5ad02ff62439363f2f31d7
Emily Dowdle <emily@editingemily.com>
2015-09-17 12:04:06 -0600

    updates to crack code functionality

--- lib/crack_code.rb
+++ lib/crack_code.rb
@@ -1,13 +1,16 @@
 class Crack_Code
 
+  CHAR_MAP = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ","]
+
   def initialize
   end
 
   def runner(array)
     final_digits = last_four_of_array(array)
-
-
-    rotation = rotate_cracked_rotations
+    rotations = find_secret_key(array, final_digits)
+    ordered_rotations = rotate_cracked_rotations(rotations, array)
+    date_offset = pull_date_offset_from_rotation(date_string)
+    key_array = subtract_date_offset_from_rotations(rotations, date_offset)
   end
 
   def last_four_of_array(array)
@@ -16,29 +19,57 @@ class Crack_Code
 
   def find_secret_key(array, final_digits)
     rotations = []
-    char_map = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ","]
-
-    array.each_with_index do |letter, index|
-      location = char_map.index(letter) # this is the rotated position
-      origin = char_map.index(final_digits[counter])
+    counter = 0
+    expected_last = [".", ".", "e", "n"]
+    final_digits.each do |letter|
+      location = CHAR_MAP.index(letter)
+      origin = CHAR_MAP.index(expected_last[counter])
+      rotations << (location - origin) % CHAR_MAP.length
+      counter += 1
+    end
+    rotations
   end
 
   def rotate_cracked_rotations(rotations, array)
     num = (array.length - 7) % 4
-    rotations.rotate(num)
+    rotations.rotate(-num)
   end
 
-  def decrypt_message_without_key(array)
-    counter = 0
-    decrypted = []
-    char_map = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ","]
-    array.each_with_index do |letter, index|
-      location = char_map.index(letter)
-      decrypted << char_map[(location - rotations[counter]) % char_map.length]
-      counter = (counter + 1) % rotations.length
-    end
-    decrypted.join
+  def pull_date_offset_from_rotation(date_string)
+    sqr = date_string.to_i ** 2
+    arr = sqr.to_s.chars.last(4)
+    arr.map {|num| num.to_i}
   end
+
+  def subtract_date_offset_from_rotations(rotation_array, offset_array)
+    counter = 0
+    key_array = []
+    rotation_array.each do |num|
+      key_array << num - offset_array[counter]
+      counter += 1
+    end
+    key_array
+  end
+
+  def final_secret_key(key_array)
+    key = []
+    string = key_array.to_s
+    key << string[0..1]
+
+    # key << string[4..5]
+    # key << string[-1]
+  end
+  # def decrypt_message_without_key(array)
+  #   counter = 0
+  #   decrypted = []
+  #   char_map = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ","]
+  #   array.each_with_index do |letter, index|
+  #     location = char_map.index(letter)
+  #     decrypted << char_map[(location - rotations[counter]) % char_map.length]
+  #     counter = (counter + 1) % rotations.length
+  #   end
+  #   decrypted.join
+  # end
 
   # def find_rotation_order(array)
   #   last_letter_rotation = string.length - 7 % 4
@@ -46,8 +77,6 @@ class Crack_Code
   # end
 
   #rotate array
-
-  rotations
 
 
 end
--- test/test_crack_code.rb
+++ test/test_crack_code.rb
@@ -1,14 +1,68 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative '../lib/decrypt'
+require_relative '../lib/crack_code'
 
-class TestEncrypt < Minitest::Test
-  def test_decrypt_string
-    code = Decrypt.new
-    string = ["h", "e"]
-    rotations = [1, 2, 3, 4]
-    assert_equal "gc", code.decrypt_message(string, rotations)
+class TestCrackCode < Minitest::Test
+  def test_find_last_four
+    code = Crack_Code.new
+    array = ["g", "o", "o", "d", "b", "y", "e", ".", ".", "e", "n", "d", ".", "."]
+
+    assert_equal [".", ".", "e", "n"], code.last_four_of_array(array)
+  end
+
+  def test_find_final_secret_key
+    code = Crack_Code.new
+    array = ["g", "o", "o", "d", "b", "y", "e", ".", ".", "e", "n", "d", ".", "."]
+    final_digits = ["o", "o", "k", "m"]
+
+    assert_equal [16, 16, 6, 38], code.find_secret_key(array, final_digits)
+  end
+  #
+  def test_find_secret_key_from_different_array
+    code = Crack_Code.new
+    array = ["g", "o", "o", "d", "b", "y", "e", ".", ".", "e", "n", "d", ".", "."]
+    final_digits = ["t", "t", "m", "u"]
+
+    assert_equal [21, 21, 8, 7], code.find_secret_key(array, final_digits)
+  end
+
+  def test_rotate_cracked_rotations
+    code = Crack_Code.new
+    array = ["g", "o", "o", "d", "b", "y", "e", ".", ".", "e", "n", "d", ".", "."]
+    rotations = [21, 21, 8, 7]
+
+    assert_equal [21, 8, 7, 21], code.rotate_cracked_rotations(rotations, array)
+  end
+
+  def test_rotate_cracked_rotations_different_array
+    code = Crack_Code.new
+    array = ["g", "d", "b", "y", "e", ".", ".", "e", "n", "d", ".", "."]
+    rotations = [21, 21, 8, 7]
+
+    assert_equal [7, 21, 21, 8], code.rotate_cracked_rotations(rotations, array)
+  end
+
+  def test_pull_date_offset
+    code = Crack_Code.new
+    rotations = [21, 21, 8, 7]
+
+    assert_equal [7, 2, 2, 5], code.pull_date_offset_from_rotation("170915")
+  end
+
+  def test_subtract_date_offset
+    code = Crack_Code.new
+    rotations = [34, 17, 52, 26]
+    offset = [7, 2, 2, 5]
+
+    assert_equal [27, 15, 50, 21], code.subtract_date_offset_from_rotations(rotations, offset)
+  end
+
+  def test_find_key
+    code = Crack_Code.new
+    key_array = [27, 15, 50, 21]
+
+    assert_equal [14, 19, 6, 2], code.final_secret_key(key_array)
   end
 
 end
